503f5b0e06420f32cb36f7115f64da96fb58301c
Alexander Berdyugin <112006440+Alexan-7@users.noreply.github.com>
2024-02-10 19:10:41 +0300

    Add files via upload

---
+++ Lesson-06_Modules/Les06_Ex03.py
@@ -0,0 +1,9 @@
+
+from sys import builtin_module_names as bnm, path as p
+import random as rnd
+
+print(bnm)
+print(*p, sep='\n')
+print(rnd.randint1, 7)
+print(path)
+#print(sys.path)
---
+++ Lesson-06_Modules/Les06_Task03.py
@@ -0,0 +1,6 @@
+from mathematical import base_math as bm
+from mathematical.advanced_math import exp
+
+x = bm.div(12, 2)
+z = exp(2, 3)
+print(x, z)
